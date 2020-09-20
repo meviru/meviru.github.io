e8e352421de8747773cd50135624f7a5b3bb2beb
Viral Patel <viralpatel508@gmail.com>
2020-09-20 18:08:53 +0530

    changes of chatsignal

--- chatsignal/js/app.js
+++ chatsignal/js/app.js
@@ -66,12 +66,12 @@ $(document).ready(function () {
     e.preventDefault();
     $(".lang-dropdown").toggleClass("active");
   });
-  
-  $(".faq-item--title").click(function(e){
+
+  $(".faq-item--title").click(function (e) {
     e.preventDefault();
     $(this).parent().toggleClass("active");
     $(this).next().slideToggle(300);
-  })
+  });
 
   var $form_modal = $(".cd-user-modal"),
     $form_login = $form_modal.find("#cd-login"),
@@ -148,4 +148,98 @@ $(document).ready(function () {
     $form_signup.removeClass("is-selected");
     $form_forgot_password.addClass("is-selected");
   }
+
+  class InfiniteSlider {
+    constructor(
+      animTime = "10000",
+      selector = ".slider",
+      container = "#slider-container"
+    ) {
+      this.slider = document.querySelector(selector);
+      this.container = document.querySelector(container);
+      this.width = 0;
+      this.oldWidth = 0;
+      this.duration = parseInt(animTime);
+      this.start = 0;
+      this.refresh = 0; //0, 1, or 2, as in steps of the animation
+      this._prevStop = false;
+      this._stop = false;
+      this._oldTimestamp = 0;
+    }
+
+    animate() {
+      /* fix for browsers who like to run JS before images are loaded */
+      const imgs = Array.prototype.slice
+        .call(this.slider.querySelectorAll("img"))
+        .filter((img) => {
+          return img.naturalWidth === 0;
+        });
+      if (imgs.length > 0) {
+        window.requestAnimationFrame(this.animate.bind(this));
+        return;
+      }
+
+      /* Add another copy of the slideshow to the end, keep track of original width */
+      this.oldWidth = this.slider.offsetWidth;
+      const sliderText =
+        '<div class="image-container slider-extra">' + this.slider.innerHTML + "</div>";
+      this.slider.innerHTML += sliderText;
+
+      /* can have content still when we move past original slider */
+      this.width = this.slider.offsetWidth;
+      const minWidth = 2 * screen.width;
+
+      /* Add more slideshows if needed to keep a continuous stream of content */
+      while (this.width < minWidth) {
+        this.slider.innerHTML += sliderText;
+        this.width = this.slider.width;
+      }
+      this.slider
+        .querySelector(".slider-extra:last-child")
+        .classList.add("slider-last");
+
+      /* loop animation endlesssly (this is pretty cool) */
+      window.requestAnimationFrame(this.controlAnimation.bind(this));
+    }
+
+    stagnate() {
+      this.container.style.overflowX = "scroll";
+    }
+
+    controlAnimation(timestamp) {
+      //console.log('this.stop: ' + this._stop + '\nthis.prevStop: ' + this._prevStop)
+      if (this._stop === true) {
+        if (this._prevStop === false) {
+          this.slider.style.marginLeft = getComputedStyle(
+            this.slider
+          ).marginLeft;
+          this._prevStop = true;
+          this._oldTimestamp = timestamp;
+        }
+      } else if (this._stop === false && this._prevStop === true) {
+        this._prevStop = false;
+        this.start = this.start + (timestamp - this._oldTimestamp);
+      } else {
+        //reset animation
+        if (this.refresh >= 1) {
+          this.start = timestamp;
+          this.slider.style.marginLeft = 0;
+          this.refresh = 0;
+          window.requestAnimationFrame(this.controlAnimation.bind(this));
+          return;
+        }
+        if (timestamp - this.start >= this.duration) {
+          this.refresh = 1;
+        }
+
+        const perc = ((timestamp - this.start) / this.duration) * this.oldWidth;
+        this.slider.style.marginLeft = -perc + "px";
+      }
+      window.requestAnimationFrame(this.controlAnimation.bind(this));
+      return;
+    }
+  }
+
+  const slider = new InfiniteSlider(35000);
+  slider.animate();
 });
